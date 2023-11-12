ae48b015dbb2bbb7f131c22c5caa7d0857d93443
danieleades <33452915+danieleades@users.noreply.github.com>
2023-11-12 21:05:41 +0000

    Add 'init' command (#3)

--- src/cli.rs
+++ src/cli.rs
@@ -1,19 +1,23 @@
 use clap::Parser;
 
+mod init;
 mod run;
 
 #[derive(Debug, Parser)]
 #[command(author, version)]
 pub enum Command {
+    /// Run GroundHog and generate new Gitlab issue recurrences
     Run(run::Command),
-    Init,
+
+    /// Set up a new GroundHog directory
+    Init(init::Command),
 }
 
 impl Command {
     pub async fn run(&self) -> anyhow::Result<()> {
         match self {
             Self::Run(command) => command.run().await,
-            Self::Init => todo!(),
+            Self::Init(command) => command.run(),
         }
     }
 }
---
+++ src/cli/init.rs
@@ -0,0 +1,49 @@
+use std::path::PathBuf;
+
+use anyhow::Ok;
+use clap::Parser;
+
+const ISSUES: &str = include_str!("issues.yml");
+const ISSUES_SCHEMA: &str = include_str!("issues.schema.json");
+
+#[derive(Debug, Parser)]
+pub struct Command {
+    /// Path to the directory that should contain the files used by GroundHog.
+    ///
+    /// The directory will be created if it doesn't exist.
+    path: PathBuf,
+}
+
+impl Command {
+    pub fn run(&self) -> anyhow::Result<()> {
+        std::fs::create_dir_all(self.path.join("templates"))?;
+        std::fs::write(self.path.join("templates/my-template.md"), "body text\n")?;
+
+        std::fs::write(self.path.join("ledger.json"), "{}")?;
+        std::fs::write(self.path.join("issues.yml"), ISSUES)?;
+        std::fs::write(self.path.join("issues.schema.json"), ISSUES_SCHEMA)?;
+
+        Ok(())
+    }
+}
+
+#[cfg(test)]
+mod tests {
+    use super::{ISSUES, ISSUES_SCHEMA};
+    use jsonschema::JSONSchema;
+    use std::fmt::Write;
+
+    #[test]
+    fn issues_schema() {
+        let schema = JSONSchema::compile(&serde_json::from_str(ISSUES_SCHEMA).unwrap()).unwrap();
+        let issues: serde_json::Value = serde_yaml::from_str(ISSUES).unwrap();
+        let result = schema.validate(&issues);
+        if let Err(errors) = result {
+            let msg: String = errors.fold(String::new(), |mut s, e| {
+                let _ = writeln!(s, "{e}");
+                s
+            });
+            panic!("{msg}");
+        }
+    }
+}
